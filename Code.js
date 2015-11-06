7ab16ebf604dfbc2bc9290640a5c8cd1146da097
Thomas M. Hinkle <tmhinkle@gmail.com>
2015-11-05 21:01:13 -0500

    Latest

--- Code.js
+++ Code.js
@@ -1,3 +1,7 @@
+function onInstall(e) {
+	onOpen();
+}
+
 function onOpen() {
   SpreadsheetApp.getUi() // Or DocumentApp or FormApp.
       .createMenu('Doctopus Folder Embedder')
@@ -19,6 +23,28 @@ function doGet(e) {
   return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME);
 } 
 
+function showAlert (title, msg) {
+	var ui = SpreadsheetApp.getUi(); // Same variations.
+	ui.alert(title,msg);
+}
+
+function confirmAction (title, message) {
+  var ui = SpreadsheetApp.getUi(); // Same variations.
+  var result = ui.alert(
+		title,
+		message,
+    ui.ButtonSet.OK_CANCEL);
+  // Process the user's response.
+  if (result == ui.Button.OK) {
+    // User clicked "Yes".
+    //ui.alert('Confirmation received.');
+		return true
+  } else {
+    // User clicked "No" or X in the title bar.
+    return false
+  }
+}
+
 function embedFoldersFromSheet () {
 	var sheet = SpreadsheetApp.getActiveSheet();
 	var range = sheet.getDataRange();
@@ -32,31 +58,39 @@ function embedFoldersFromSheet () {
 			row[5]=='Exclude'
 		 ) {
 		// Ok -- let's do this thing...
-		if (row[6] != 'Completed') {
-			sheet.getRange(1,6).setValue('Completed'); // Set up row to track if we are done yet...
+		if 	(confirmAction(
+			'Embed folders?',
+			'Put student folders from "'+row[3]+'" inside of folders from "'+row[4]+'?')) {
+			if (row[6] != 'Completed') {
+				sheet.getRange(1,6).setValue('Completed'); // Set up row to track if we are done yet...
+			}
+			range.getValues().forEach( function (row,idx) {
+				if (idx!=0) {
+					if (row[5] != 'Exclude' && row[6] != 'Done') {
+						try {
+							parentFolder = DriveApp.getFolderById(row[4])
+							childFolder = DriveApp.getFolderById(row[3])
+							parentFolder.addFolder(childFolder);
+							range.getCell(idx+1,7).setValue('Done');
+						}
+						catch (err) {
+							cell = range.getCell(idx+1,7)
+							cell.setValue(JSON.stringify(err));
+							cell.setFontColor('#ff0000');
+						} // end try/catch
+					} // end if not excluded or done
+				} // end if not header row
+			} 
+															 )// end forEach row
+		} // end if confirmed
+		else {
+			showAlert('Cancelled','Not moving folders after all: you cancelled the action.');
 		}
-		range.getValues().forEach( function (row,idx) {
-			if (idx!=0) {
-				if (row[5] != 'Exclude' && row[6] != 'Done') {
-					try {
-						parentFolder = DriveApp.getFolderById(row[4])
-						childFolder = DriveApp.getFolderById(row[3])
-						parentFolder.addFolder(childFolder);
-						range.getCell(idx+1,7).setValue('Done');
-					}
-					catch (err) {
-						cell = range.getCell(idx+1,7)
-						cell.setValue(JSON.stringify(err));
-						cell.setFontColor('#ff0000');
-					} // end try/catch
-				} // end if not excluded or done
-			} // end if not header row
-		} 
-														 )// end forEach row
 	} // end if things look good
 	else { // else if things look borked...
 		Logger.log('Data looks fishy...');
 		Logger.log('Row: '+JSON.stringify(row));
+		showAlert('Not ready','Spreadsheet does not appear to have roster data loaded. Make sure you\'ve selected rosters and clicked "Load Rosters."');
 		throw "Header row doesn't look right. Maybe try re-loading rosters?";
 	} // end else
 }
@@ -76,44 +110,53 @@ function updateRostersOnSheet (childRosterObj, parentRosterObj) {
 	) // end forEach childStudent
 	// Now toPush should have an array with the students that are in
 	// both classes...
-	sheet = SpreadsheetApp.getActiveSheet();
-	sheet.clear(); // Is this a good idea?
-	sheet.appendRow([
-		'First',
-		'Last',
-		'Email',
-		'Child Folder ('+childRosterObj.className+')',
-		'Parent Folder ('+parentRosterObj.className+')',
-		'Exclude',
-		'Completed',
-	  ]);
-	toPush.forEach(
-		function (obj) {
-			childStudent = obj[0];
-			parentStudent = obj[1];
-			if (childStudent.email==parentStudent.email) {
-				// Just making sure -- this should be true anyway...
-				sheet.appendRow([
-					childStudent.firstName,
-					childStudent.lastName,
-					childStudent.email,
-					childStudent.classFolder,
-					parentStudent.classFolder,])
-			}
-			else {
-				Logger.log('Odd -- child and parent not a match:'+JSON.stringify(childStudent)+' and '+JSON.stringify(parentStudent))
-			}
-		}) // end forEach child/parent
-	// Set up formatting and such
-	var excludeValidator = SpreadsheetApp.newDataValidation().requireValueInList(['Exclude',''],true);
-	var lastRow = sheet.getDataRange().getLastRow()
-	sheet.getRange(2,6,lastRow-1,1).setDataValidation(excludeValidator); // get exclude range...
-	var header = sheet.getRange('A1:G1')
-	header.setFontWeight('bold');
-	header.setBackground('#000000');
-	header.setFontColor('#ffffff');
-	sheet.setFrozenRows(1);
-	return true;
+	if (toPush.length==0) {
+		showAlert('No students found','There are no students in common between '+childRosterObj.className+' ('+childRosterObj.students.length+' students) and '+parentRosterObj.className+' ('+parentRosterObj.students.length+')');
+		return false
+	}
+	else {
+		if (confirmAction('Load student data?','Warning: loading student data will clear the current spreadsheet. Continue?')) {
+			sheet = SpreadsheetApp.getActiveSheet();
+			sheet.clear(); // Is this a good idea?
+			sheet.appendRow([
+				'First',
+				'Last',
+				'Email',
+				'Child Folder ('+childRosterObj.className+')',
+				'Parent Folder ('+parentRosterObj.className+')',
+				'Exclude',
+				'Completed',
+			]);
+			toPush.forEach(
+				function (obj) {
+					childStudent = obj[0];
+					parentStudent = obj[1];
+					if (childStudent.email==parentStudent.email) {
+						// Just making sure -- this should be true anyway...
+						sheet.appendRow([
+							childStudent.firstName,
+							childStudent.lastName,
+							childStudent.email,
+							childStudent.classFolder,
+							parentStudent.classFolder,])
+					}
+					else {
+						Logger.log('Odd -- child and parent not a match:'+JSON.stringify(childStudent)+' and '+JSON.stringify(parentStudent))
+					}
+				}) // end forEach child/parent
+			// Set up formatting and such
+			var excludeValidator = SpreadsheetApp.newDataValidation().requireValueInList(['Exclude',''],true);
+			var lastRow = sheet.getDataRange().getLastRow()
+			sheet.getRange(2,6,lastRow-1,1).setDataValidation(excludeValidator); // get exclude range...
+			var header = sheet.getRange('A1:G1')
+			header.setFontWeight('bold');
+			header.setBackground('#000000');
+			header.setFontColor('#ffffff');
+			sheet.setFrozenRows(1);
+			return true;
+		} // end if confirmAction...
+		return false;
+	} // end if there are students to push...
 } // end updateRostersOnSheet
 
 
@@ -124,9 +167,10 @@ function getHomeGlobal() {
     Logger.log('getHomeGlobal')
     var rosters = DriveRoster.getRosters();
     Logger.log('got rosters')
-    var classRoomRosterNames = getClassroomRosterNames();    
-    Logger.log('classroom rosters...')
-    rosters = rosters.concat(classRoomRosterNames);
+		// We don't want classroom rosters since they don't have folders
+    //var classRoomRosterNames = getClassroomRosterNames();    
+    //Logger.log('classroom rosters...')
+    //rosters = rosters.concat(classRoomRosterNames);
     rosters.sort(function(a, b) {
       var textA = a.className.toUpperCase();
       var textB = b.className.toUpperCase();
